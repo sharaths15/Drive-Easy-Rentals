ed8b7555ca9186b01e0165d3bd7c29e11758c94f
yanaha405 <premvishaL595@gmail.com>
2025-01-22 21:29:12 +1100

    Made some changes in rentaldb.sql

--- rentaldb.sql
+++ rentaldb.sql
@@ -1,11 +1,7 @@
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 26, 2024 at 02:02 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
